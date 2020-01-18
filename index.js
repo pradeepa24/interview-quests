359770a5c18ab234bea032e8d61cc115f81ac4bf
pradeepa24 <sh.pradeepa01@gmail.com>
2020-01-18 15:31:51 -0500

    added functions

--- index.js
+++ index.js
@@ -79,64 +79,65 @@ Output 7:
     
 */
 function patternMatching(str1,str2){
-    let arr = [];
-     if(str1 === str2){
-       return 1;
-     } else{
-         if((str2.indexOf("*") === -1) && (str2.indexOf("?") === -1)){
-           return 0;
-         } else {
-           for(let i=0;i<str1.length;i++){
-             for(let j=0;j<str2.length;j++){
-               if(i===j){
-                 if(str1[i] !== str2[j]){
-                   if(str2[j]==="?"){
+  let arr = [];
+   if(str1 === str2){
+     return 1;
+   } else{
+       if((str2.indexOf("*") === -1) && (str2.indexOf("?") === -1)){
+         return 0;
+       } else {
+         for(let i=0;i<str1.length;i++){
+           for(let j=0;j<str2.length;j++){
+             if(i===j){
+               if(str1[i] !== str2[j]){
+                 if(str2[j]==="?"){
                    arr.push(str1[i]);
-                   }else if(str2[j] === "*"){
-                      if(j === str2.length -1){
-                         return 1;
-                       } else{
-                         arr.push(str1[i]);
-                         let indStr1 = i;
-                         let indStr2 = j;
-                         if(indStr1< str1.length){
-                           indStr2++;
+                 }else if(str2[j] === "*"){
+                    if(j === str2.length -1){
+                       return 1;
+                     } else{
+                       arr.push(str1[i]);
+                       let indStr1 = i;
+                       let indStr2 = j;
+                        while((str2.indexOf("*")!==-1) &&(indStr1< str1.length-1)){
+                         indStr2++;
+                         indStr1++;
+                         while((str1[indStr1]!== str2[indStr2])&& (indStr1 < str1.length)){
+                           arr.push(str1[indStr1]);
                            indStr1++;
-                           while((str1[indStr1]!== str2[indStr2])&& (indStr1 < str1.length)){
-                             arr.push(str1[indStr1]);
-                             indStr1++;
-                           }
-                          if(str1[indStr1] === str2[indStr2]){
-                            arr.push(str1[indStr1])
-                          }
-                          }
+                         }
+                        if(str1[indStr1] === str2[indStr2]){
+                          arr.push(str1[indStr1])
                         }
-                     }else{
-                       return 0;
-                     }
-                 } else{
-                   arr.push(str1[i]);
-                 }
+                        str2 = str2.substring(indStr2,str2.length-1);
+                        }
+                      }
+                  }else{
+                     return 0;
+                  }
+               } else{
+                 arr.push(str1[i]);
                }
              }
            }
-           if(arr.join(",").replace(/,/g,"") === str1){
-             return 1;
-           }else{
-             return 0;
-           }
-        }
-     }
-  }
-  
-    console.log("pattern match for aa ,a : "+patternMatching("aa","a"));
-    console.log("pattern match for aa ,aa : "+patternMatching("aa","aa"));
-    console.log("pattern match for aaa ,aa : "+patternMatching("aaa","aa"));
-    console.log("pattern match for aa ,* : "+patternMatching("aa","*"));
-    console.log("pattern match for aa ,a* : "+patternMatching("aa","a*")); 
-    console.log("pattern match for ab ,?* : "+patternMatching("ab","?*"));
-    console.log("pattern match for aab ,c*a*b : "+patternMatching("aab","c*a*b"));
-  
+         }
+         if(arr.join(",").replace(/,/g,"") === str1){
+           return 1;
+         }else{
+           return 0;
+         }
+      }
+   }
+}
+
+  console.log("pattern match for aa ,a : "+patternMatching("aa","a"));
+  console.log("pattern match for aa ,aa : "+patternMatching("aa","aa"));
+  console.log("pattern match for aaa ,aa : "+patternMatching("aaa","aa"));
+  console.log("pattern match for aa ,* : "+patternMatching("aa","*"));
+  console.log("pattern match for aa ,a* : "+patternMatching("aa","a*")); 
+  console.log("pattern match for ab ,?* : "+patternMatching("ab","?*"));
+  console.log("pattern match for aab ,c*a*b : "+patternMatching("aab","c*a*b"));
+  console.log("pattern match for aabaacb ,a*a*b : "+patternMatching("aabaacb","a*a*b"));
   /*
   
   ++++++++
@@ -217,4 +218,97 @@ function patternMatching(str1,str2){
   }
   console.log("sports check for [5,2,C,D,+] : "+sports(["5","2","C","D","+"]));
   
-   
+  function bonAppetit(bill, k, b) {
+    res = 0;
+    bill.forEach((b,i)=>{
+     if(i !== k){
+       res += b;
+     }
+    })
+    if(res/2 === b){
+        console.log("Bon Appetit");
+    }else{
+        console.log(b-(res/2));
+    }
+  }
+  bonAppetit([3,10,2,9],1,7);
+  bonAppetit([3,10,2,9],1,12);
+  function breakingRecords(scores) {
+    let highScoreArr = [];
+    let lowScoreArr = [];
+    let highScore = scores[0];
+    let lowScore = scores[0];
+    //console.log(scores)
+    for(let i=1;i<=scores.length;i++){
+     // console.log(scores[i])
+      if(highScore < scores[i]){
+        highScoreArr.push(scores[i]);
+        highScore = scores[i];
+      } else if(lowScore > scores[i]){
+        lowScoreArr.push(scores[i]);
+        lowScore = scores[i];
+      }
+      
+    }
+    return [highScoreArr.length,lowScoreArr.length];
+    }
+    console.log(breakingRecords([10,5,20,20,4,5,2,25,1]))
+    
+    function birthday(s, d, m) {
+      let sum = 0;
+      let j;
+      let track = 1;
+      let sumArr = [];
+      for(let i=0;i<s.length;i++){
+        j = i;
+        while((track<=m)&&(j<s.length)){
+         sum += s[j];
+         j++;
+         track++;
+        }
+        track = 1;
+        sumArr.push(sum);
+        sum = 0;
+      }
+    return sumArr.filter((su,ind)=>su === d).length;
+    }
+    console.log(birthday([2,5,1,3,4,4,3,5,1,1,2,1,4,1,3,3,4,2,1],18,7));
+    function divisibleSumPairs(n, k, ar) {
+      let j;
+      let sum;
+      let resArr = [];
+      ar.forEach((el,ind)=>{
+        sum = ar[ind];
+        j = ind+1;
+      while(j<n){
+         sum += ar[j];
+         resArr.push(sum);
+         j++;
+         sum=ar[ind];
+        }
+      })
+      return (resArr.filter((res)=>(res%k===0)).length)
+    }
+    console.log(divisibleSumPairs(6,3,[1,3,2,6,1,2]))
+    ;
+    function migratoryBirds(arr) {
+      let resArr = [];
+      let result;
+      arr.forEach((el,ind)=>{
+        if(!(resArr.find((n,i)=>n.num === el))){
+          resArr.push({num:el,count:1})
+        } else{
+          resArr[resArr.findIndex((n)=>n.num === el)].count++;
+        } 
+      })
+    resArr.sort((a,b)=>(a.count-b.count));
+    let dupArr=resArr.filter((res)=>res.count === resArr[resArr.length-1].count);
+    if(dupArr.length > 1){
+      dupArr.sort((a,b)=>(a.num - b.num));
+      result = dupArr[0].num;
+    }else{
+      result = resArr[resArr.length-1].num;
+    }
+    return result;
+    }
+    console.log(migratoryBirds([1,2,3,4,5,4,3,2,1,3,4]));  
